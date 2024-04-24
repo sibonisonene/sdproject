f55c31b8db50f972ccb1ea3e84010caf69c0c243
siboniso nene <sibonisodev@gmail.com>
2024-04-24 10:50:46 +0200

    init

---
+++ .github/src/components/auth/SignUp.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { Link, useHistory } from "react-router-dom";
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        history.push("/components/Dashboard");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="sign-in-container">
+      <form onSubmit={signUp}>
+        <h1>Register</h1>
+        <p>EMAIL ADDRESS *</p>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <p>PASSWORD *</p>
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+        <div className="link-to-register">
+          Already have an account?
+          <Link to="/">Log In</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
