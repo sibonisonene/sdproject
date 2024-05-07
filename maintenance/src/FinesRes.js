33d024aae5b88d469a4c55961a59a54914f81c61
Jony Phiri <2456518@students.wits.ac.za>
2024-05-07 19:41:42 +0200

    Add resident fines page

---
+++ maintenance/src/FinesRes.js
@@ -0,0 +1,49 @@
+import "./index.css";
+import { useState } from "react";
+
+function FinesRes() {
+
+    //Get the issues from the database
+    //Use stub
+    const[fines, setFines] = useState([
+        {
+            id: 1,
+            admin: 5,
+            resident: 18,
+            amount: 300,
+            status: "Due",
+            description: "Resident has been found smoking in a non-smoking area"
+        },
+        {
+            id: 2,
+            admin: 5,
+            resident: 18,
+            amount: 300,
+            status: "Paid",
+            description: "Resident has been found smoking in a non-smoking area"
+        }
+    ]);
+
+
+
+    return (
+        <article className="FinesRes" data-testid="FinesRes">
+            <h1>Your Fines</h1>
+            {
+                fines.map((fine) => {
+                    return <section className="FineItem" key={fine.id}>
+                        <section>
+                            <h2>{fine.description}</h2>
+                            <h3>issued by: {fine.admin}</h3>
+                        </section>
+                        <button>Pay</button>
+                    </section>
+                   
+                })
+            }
+        </article>
+    )
+
+}
+
+export default FinesRes;
