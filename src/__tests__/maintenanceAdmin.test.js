a63dc80816443d9a6d94270894aece837c1ae927
Jony Phiri <2456518@students.wits.ac.za>
2024-05-08 14:08:43 +0200

    implement testing

---
+++ src/__tests__/maintenanceAdmin.test.js
@@ -0,0 +1,41 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import App from '../App';
+
+//release resources after each test
+afterEach(() => {
+    cleanup();
+});
+
+//test if the componant is rendered
+test('app component should be in the document', () => {
+    render(<App/>);
+    const app = screen.getByTestId('IssuesAdmin');
+    //Check if app is in the body of the document
+    expect(app).toBeInTheDocument();
+});
+
+// //test if status update works
+// test('statusClick_anyState_buttonClick', () => {
+//     render(<App/>);
+//     const oldState = screen.getByTestId("Status");
+//     //Click the button
+//     const newState = screen.getByTestId('Status');
+//     expect(oldState).not.toBe(newState);
+// });
+
+// //test if status update works
+// test('feebackClick_anyState_buttonClick', () => {
+//     render(<App/>);
+//     //onClick
+//     const oldFeedback = screen.getByTestId('Feedback');
+
+//     //afterClick check with the database
+//     const newFeedback = screen.getByTestId('Feedback');
+//     expect(oldFeedback).toBe(newState);
+// });
+
+
+//test if feedback update works
+
+
+//IMPORTANT change the import App to the component name
