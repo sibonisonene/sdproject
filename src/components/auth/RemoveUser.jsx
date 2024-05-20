d9123792e7309c6c43bd1f02f9277042fc0f9c88
Ahmad <ahmadg0323@gmail.com>
2024-05-20 11:53:40 +0200

    remove a profile from db

--- src/components/auth/RemoveUser.jsx
+++ src/components/auth/RemoveUser.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import { deleteUser } from '../../firebase'; 
+import useDeleteProfile from '../hooks/useDeleteProfile';
+import { auth } from '../../firebase';
+import AuthDetails from '../AuthDetails';
 
 const RemoveUser = () => {
   const [email, setEmail] = useState('');
+  const { deleteProfile } = useDeleteProfile();
 
   const handleRemoveUser = async (e) => {
     e.preventDefault();
     try {
       const result = await deleteUser(email);
+      await deleteProfile(email);
+      console.log("Profile deleted successfully");
       alert(result.message); 
     } catch (error) {
       alert(`Error: ${error.message}`); 
